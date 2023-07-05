afc80cd6c4d1fd4756fb6026192187e1087561d3
Louis Sanna <louis.sanna@ekimetrics.com>
2023-07-05 19:46:39 +0200

    feat(reviews): display reviews

--- app/reviews/page.tsx
+++ app/reviews/page.tsx
@@ -1,47 +1,31 @@
 import { StarIcon } from '@heroicons/react/20/solid'
 import { format } from 'date-fns'
+import rawReviews from '../../data/processed/reviews.json'
 
-const rawReviews = [
-  {
-    docs: 'food was good could have been hotter. server,kim, was awesome. she was friendly. knew the menu and seemed to enjoy what she was doing',
-    rating: '4.0',
-    timestamp: '2021-12-05 14:41:00'
-  },
-  {
-    docs: "traveling and our hotel recommended it since i love italian food! it was so good, we went back a second time. the portions are huge though, i couldn't even get through half mine after sharing a caesar.",
-    rating: '5.0',
-    timestamp: '2021-10-14 18:13:00'
-  },
-  {
-    docs: 'very good experience. staff were attentive and food was great.',
-    rating: '5.0',
-    timestamp: '2022-02-07 21:47:00'
-  }
-]
+interface RawReview {
+  docs: string
+  rating: string
+  timestamp: string
+}
 
-const reviews = {
-  average: 4,
-  totalCount: 1624,
-  counts: [
-    { rating: 5, count: 1019 },
-    { rating: 4, count: 162 },
-    { rating: 3, count: 97 },
-    { rating: 2, count: 199 },
-    { rating: 1, count: 147 }
-  ],
-  featured: [
-    {
-      id: 1,
-      rating: 5,
-      content: `
-        <p>This is the bag of my dreams. I took it on my last vacation and was able to fit an absurd amount of snacks for the many long and hungry flights.</p>
-      `,
-      author: 'Emily Selman',
-      avatarSrc:
-        'https://images.unsplash.com/photo-1502685104226-ee32379fefbe?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=256&h=256&q=80'
-    }
-    // More reviews...
-  ]
+const reviews = processRawReviews(rawReviews)
+
+interface ReviewCount {
+  rating: number
+  count: number
+}
+
+interface FeaturedReview {
+  id: number
+  rating: number
+  content: string
+}
+
+interface Reviews {
+  average: number
+  totalCount: number
+  counts: ReviewCount[]
+  featured: FeaturedReview[]
 }
 
 function classNames(...classes: any[]) {
@@ -84,43 +68,47 @@ export default function ReviewPage() {
             <h3 className="sr-only">Review data</h3>
 
             <dl className="space-y-3">
-              {reviews.counts.map(count => (
-                <div key={count.rating} className="flex items-center text-sm">
-                  <dt className="flex flex-1 items-center">
-                    <p className="w-3 font-medium text-gray-900">
-                      {count.rating}
-                      <span className="sr-only"> star reviews</span>
-                    </p>
-                    <div
-                      aria-hidden="true"
-                      className="ml-1 flex flex-1 items-center"
-                    >
-                      <StarIcon
-                        className={classNames(
-                          count.count > 0 ? 'text-yellow-400' : 'text-gray-300',
-                          'h-5 w-5 flex-shrink-0'
-                        )}
+              {reviews.counts
+                .sort((a, b) => b.rating - a.rating)
+                .map(count => (
+                  <div key={count.rating} className="flex items-center text-sm">
+                    <dt className="flex flex-1 items-center">
+                      <p className="w-3 font-medium text-gray-900">
+                        {count.rating}
+                        <span className="sr-only"> star reviews</span>
+                      </p>
+                      <div
                         aria-hidden="true"
-                      />
+                        className="ml-1 flex flex-1 items-center"
+                      >
+                        <StarIcon
+                          className={classNames(
+                            count.count > 0
+                              ? 'text-yellow-400'
+                              : 'text-gray-300',
+                            'h-5 w-5 flex-shrink-0'
+                          )}
+                          aria-hidden="true"
+                        />
 
-                      <div className="relative ml-3 flex-1">
-                        <div className="h-3 rounded-full border border-gray-200 bg-gray-100" />
-                        {count.count > 0 ? (
-                          <div
-                            className="absolute inset-y-0 rounded-full border border-yellow-400 bg-yellow-400"
-                            style={{
-                              width: `calc(${count.count} / ${reviews.totalCount} * 100%)`
-                            }}
-                          />
-                        ) : null}
+                        <div className="relative ml-3 flex-1">
+                          <div className="h-3 rounded-full border border-gray-200 bg-gray-100" />
+                          {count.count > 0 ? (
+                            <div
+                              className="absolute inset-y-0 rounded-full border border-yellow-400 bg-yellow-400"
+                              style={{
+                                width: `calc(${count.count} / ${reviews.totalCount} * 100%)`
+                              }}
+                            />
+                          ) : null}
+                        </div>
                       </div>
-                    </div>
-                  </dt>
-                  <dd className="ml-3 w-10 text-right text-sm tabular-nums text-gray-900">
-                    {Math.round((count.count / reviews.totalCount) * 100)}%
-                  </dd>
-                </div>
-              ))}
+                    </dt>
+                    <dd className="ml-3 w-10 text-right text-sm tabular-nums text-gray-900">
+                      {Math.round((count.count / reviews.totalCount) * 100)}%
+                    </dd>
+                  </div>
+                ))}
             </dl>
           </div>
         </div>
@@ -166,4 +154,53 @@ export default function ReviewPage() {
       </div>
     </div>
   )
+}
+
+function processRawReviews(raws: RawReview[]) {
+  let totalCount = raws.length
+  let sum = 0
+  let ratingCounts = { '1': 0, '2': 0, '3': 0, '4': 0, '5': 0 }
+
+  let featured = raws.slice(0, 100).map((review, index) => ({
+    id: index + 1,
+    rating: parseRating(review.rating),
+    content: `<p>${review.docs}</p>`,
+    timestamp: review.timestamp
+  }))
+
+  for (let review of raws) {
+    const rating = parseRating(review.rating)
+    sum += rating
+    // @ts-ignore
+    ratingCounts[rating] += 1
+  }
+
+  let average = sum / totalCount
+
+  let counts = Object.keys(ratingCounts).map(rating => ({
+    rating: parseRating(rating),
+    // @ts-ignore
+    count: ratingCounts[rating]
+  }))
+
+  return {
+    average: Math.round(average * 10) / 10,
+    totalCount,
+    counts,
+    featured
+  }
+}
+
+function parseRating(ratingStr: string): number {
+  // Parse rating to float and round to nearest integer
+  let parsedRating = Math.round(parseFloat(ratingStr))
+
+  // Check if parsedRating is within [1, 2, 3, 4, 5]
+  if (parsedRating < 1) {
+    return 1
+  } else if (parsedRating > 5) {
+    return 5
+  } else {
+    return parsedRating
+  }
 }
