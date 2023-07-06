04186f2b849047836985c31efde9e52f31a4f731
Louis Sanna <louis.sanna@ekimetrics.com>
2023-07-06 02:39:09 +0200

    feat(topics): add charts

--- app/topics/page.tsx
+++ app/topics/page.tsx
@@ -1,12 +1,43 @@
 export default function TopicsPage() {
   return (
-    <div className="relative flex items-center justify-center h-screen">
-      <iframe
-        src="/topic_viz.html"
-        className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full h-[90vh]"
-        title="Topic Visualization"
-        style={{ width: '1200px' }}
-      />
+    <Container>
+      <Card>
+        <iframe
+          src="/topic_viz.html"
+          title="Topic Visualization"
+          style={{ width: '1150px', height: '770px' }}
+        />
+      </Card>
+      <Card>
+        <iframe
+          src="/count_macro_macro_per_month.html"
+          title="Topic Visualization"
+          style={{ width: '1150px', height: '770px' }}
+        />
+      </Card>
+      <Card>
+        <iframe
+          src="/mean_macro_macro_per_month.html"
+          title="Topic Visualization"
+          style={{ width: '1150px', height: '770px' }}
+        />
+      </Card>
+    </Container>
+  )
+}
+
+function Card({ children }: any) {
+  return (
+    <div className="overflow-hidden rounded-lg bg-white shadow mt-8">
+      <div className="mt-4 px-4 py-2 sm:p-6 flex justify-center">
+        {children}
+      </div>
     </div>
+  )
+}
+
+function Container({ children }: any) {
+  return (
+    <div className="mx-auto max-w-8xl sm:px-6 lg:px-8 py-8">{children}</div>
   )
 }
